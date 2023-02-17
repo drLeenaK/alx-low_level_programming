0fd37ccec833836e4467bfed309a12e492eeed83
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-02-17 13:39:39 -0800

    datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long long int c;
 float d;
 char e;
 
-printf("Size of a chart : %lu byte(s)\n", (unsigned long)sizeof(e))
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a))
-printf("Size of an long int: %lu byte(s)\n", (unsigned long)sizeof(b))
-printf("Size of an long long int: %lu byte(s)\n", (unsigned long)sizeof(c))
-printf("Size of an float: %lu byte(s)\n", (unsigned long)sizeof(d))
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
 return (0);
 }
