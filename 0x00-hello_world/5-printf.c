ad30b55e7ab3bc7abb17c9e8e8a3d18860d07285
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-02-17 12:46:26 -0800

    printing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - Hello printf
+ *main - printingdifferently
  *
  *Return: Always 0
  */
-int main(0)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
