98e70f7333b1529875f7ab5fafd7a2d75cae7238
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-02-22 12:15:34 -0800

    9times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - prints time table
+ *
+ */
+void times_table(void)
+{
+	int num, mul, pro;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mul = 1; mul <= 9; mul++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+		pro = num * mul;
+		if (pro <= 9)
+			_putchar(' ');
+		else
+			_putchar((pro / 10) + '0');
+		_putchar((pro % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
