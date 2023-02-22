916058371f27ac61197ec0ef3bc6fdafa1d36d73
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-02-21 19:23:58 -0800

    tentimeslowercasealpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - tentimes lower case alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+	char ch;
+	int i = 0;
+
+	while (i++ <= 9)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
