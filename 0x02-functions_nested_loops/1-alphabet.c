c144419dd85325ee4f08b6a04f9e99accc60db99
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-02-21 17:31:27 -0800

    printalphabetin lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void);
+{
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar("\n");
+return (0);
+}
