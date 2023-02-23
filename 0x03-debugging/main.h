3bcf358c30fa5a312a05cfdf1a182e8b9dfc9d66
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-02-23 14:06:34 -0800

    multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative - tests whether + or -
+ * @i: will be checked
+ *
+ * Description: Declarations for all functions that will be used
+ */
+void positive_or_negative(int i)
+
+
+#endif
