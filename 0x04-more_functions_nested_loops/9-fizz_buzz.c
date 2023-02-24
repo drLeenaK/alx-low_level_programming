12f8dbb00f050f3bab59da76ed26983b529261ae
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-02-24 14:47:00 -0800

    header and character

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	int i
-	
-	for (i = 1; 1 <= 100
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,21 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- *
- * Description: Declarations for all functions that will be used
- */
-
-int _isupper(int c)
-int _isdigit(int c)
-int mul(int a, int b)
-void print_numbers(void)
-void print_most_numbers(void)
-void more_numbers(void)
-void print_line(int n)
-void print_diagonal(int n)
-void print_square(int size)
-void print_triangle(int size)
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
