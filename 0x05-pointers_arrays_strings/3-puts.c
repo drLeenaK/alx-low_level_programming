2720bd845153fce4e5d2c996632deab06864fb50
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-02-27 14:00:20 -0800

    reverses

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print string, new line, to stdout
+ * @str: will be checked
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar(6'\n');
+}
