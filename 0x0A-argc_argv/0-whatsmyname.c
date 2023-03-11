3957e2a8eba2191ad9b1d3158f216cc33d2319de
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-03-11 11:31:16 -0800

    prints name followed by new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - printing a name followed by a new  line
+ * @argc: count commands
+ * @argv: vector commands
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+arguments counters and arguments vectors
