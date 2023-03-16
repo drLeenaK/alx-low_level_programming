6efa9c24acd8794b8a972e07b653ac5bef8b387f
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-03-16 12:21:14 -0700

    two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,59 +1,48 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: string n
- * Return: as string value
- */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-
-/**
- * string_nconcat - function that concatenates two strings
+ * string_nconcat - function definition
  * @s1: the first string
  * @s2: the second string
- * @n: the n string
- * Description: concatenates s1 and s2
- * Return: the concatenated string
+ * @n: the number of bytes for s2
+ * Description: point to a newly allocated memory in space
+ * which contains s1
+ * Return: a string or NULL
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
-	char *new_string;
-	int index;
-	int nconcat_index = 0;
-	unsigned int index;
-	int len = 0;
-
-	if (s1 == NULL)
-		s1 = "";
+	unsigned int index, len1, len2;
+	char *string;
 
 	if (s2 == NULL)
 		s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-	if (n >= s2)
-		s1 = "s2";
+	len1 = 0;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+	while (s1[len1] != '\0')
+		len1++;
 
-	for (index = 0; s1[index] || s2[index] || n[index]; index++)
-		len++;
+	if (n >= len2)
+		n = len2;
 
-	new_string  = malloc(sizeof(char) * len);
-
-	if (new_string == NULL)
+	string = malloc(sizeof(char) * n + len1 + 1);
+	if (string == NULL)
 		return (NULL);
 
-	for (index = 0; s1[index]; index++)
-		new_string[nconcat_index++] = s1[index];
+	for (index = 0; index < len1; index++)
+		string[index] = s1[index];
 
-	for (index = 0; s2[index]; index++)
-		new_string[nconcat_index++] = s2[index];
+	for (index = 0; index < n; index++)
+		string[index + len1] = s2[index];
 
-	for (index = 0; n[index]; index++)
-		new_string[nconcat_index++] = s2[index];
+	string[index + len1] = '\0';
 
-	return (new_string);
+	return (string);
 }
