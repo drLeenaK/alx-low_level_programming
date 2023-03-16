107747440a727f4fbb70cf35e31736c75a2f4016
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-03-16 10:46:39 -0700

    allocate memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,23 +5,16 @@
 /**
  * malloc_checked - function that allocates memory
  * @b: initialize the interger
- * Return: a pointer
+ * Return: a pointer SUCCESS 98 fails
  */
 
 void *malloc_checked(unsigned int b)
 
 {
-	int *b;
+	int *ptr;
 
-	b = malloc(sizeof(b));
-}
-/**
- * malloc_checked - allocates memory
- * @b: initialize the interger
- * Return: 98 fails
- */
-void *malloc_checked(unsigned int b)
-{
+	ptr = malloc(b);
+
 	if (b == NULL)
 	exit(98);
 
