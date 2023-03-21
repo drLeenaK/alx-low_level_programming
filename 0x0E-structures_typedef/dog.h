b485903c498fd84f657ffe0fa1abd6d114658f9c
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-03-20 18:26:35 -0700

    dog details

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * struct dog - description of a dog
@@ -16,4 +16,11 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _putchar(char);
+typedef struct dog dog_t;
+
 #endif
