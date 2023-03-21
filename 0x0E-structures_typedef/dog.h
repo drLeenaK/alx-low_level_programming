aeb43e9fa347b23ceef8a74a17ada74b814154c6
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-03-20 17:48:21 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef "main.h"
+#define "main.h"
+
+/**
+ * init_dog - description of a dog
+ * @d: type
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ *
+ * Description: Longer description
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	char *type;
+	float *age;
+	char *owner;
+};
+
+int main(void)
+{
+	struct dog;
+
+	return (0);
+}
+
