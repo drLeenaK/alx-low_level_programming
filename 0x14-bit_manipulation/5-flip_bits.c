0bcc9902f6084d16d664846e56f4117d4cf5c9e5
Leena Ngonyofi Kanandjebo <lvilma204@gmail.com>
2023-04-08 20:01:50 -0700

    flipping n and m

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,10 +15,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int i = n ^ m;
 	unsigned int count = 0;
 
-	while i
-	{
-		count++;
-		i &= (i - 1);
-	}
+	while (i)
+		count += i & 1;
+		i >>= 1;
+
 	return (count);
 }
